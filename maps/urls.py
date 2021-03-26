b04e1026585f6991dc0ff81dc14817316d8b23a0
Esmail <smal137800@gmail.com>
2021-03-26 17:16:30 +0430

    added or changed to, home view, login view, logout view.

--- maps/urls.py
+++ maps/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, login_view, profile_view, logout_view
 
 urlpatterns = [
     path("", home_view, name="home"),
+    path("login/", login_view, name="login"),
+    path("profile/", profile_view, name="profile"),
+    path("logout/", logout_view, name="logout"),
 ]
